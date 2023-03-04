8e721f11d8a3a9d99eb5e339e01690f66ec64852
endrik koppel <ekoppel@tktk.ee>
2023-03-04 23:41:29 +0200

    add week 4 code

---
+++ For_Loop_Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿int sum = 0;
+
+
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($" Current total: {sum}");
+    sum=sum+i;//sum+=i;
+}
+Console.WriteLine($"Final total {sum}");
+Console.WriteLine("------------------------------------------------------------------------------------------");
+//for (int i = 10; i > 0; i--)
+//{
+  //  Console.WriteLine($"i={i-1}");
+//}
